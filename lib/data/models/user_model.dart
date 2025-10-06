c42c4ebc88120e3b4adc48e119301e0a204a1ae1
Ghofran Malkat <gh.malket@gmail.com>
2025-10-06 10:06:17 +0200

    Add formJson and toJson functions to User Model

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -4,6 +4,29 @@ class User {
   String password;
   String role;
   String number;
+  String? token;
 
-  User({required this.name, required this.email, required this.password, required this.role, required this.number});
+  User({required this.name, required this.email, required this.password, required this.role, required this.number,this.token});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      password: json['password'] ?? '',
+      role: json['role'] ?? '',
+      number: json['number'] ?? '',
+      token: json['token'] ?? ''
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'role': role,
+      'number': number,
+      'token': token
+    };
+  }
 }

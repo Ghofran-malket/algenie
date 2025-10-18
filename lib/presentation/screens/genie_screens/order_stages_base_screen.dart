70053a0b1e2ee9b25124acc4d14bb8d55c061dcc
Ghofran Malkat <gh.malket@gmail.com>
2025-10-18 18:29:50 +0200

    Fix padding in order stages base screen

--- lib/presentation/screens/genie_screens/order_stages_base_screen.dart
+++ lib/presentation/screens/genie_screens/order_stages_base_screen.dart
@@ -19,118 +19,115 @@ class OrderStagesBaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          
-          //title
-          Center(
-            child: Text(
-              "$title ${store.name}",
-              style: TextStyle(
-                fontFamily: "Poppins-Medium",
-                fontSize: 20,
-                color: Color(0xFF000000),
+      body: Padding(
+      padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(17),),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            
+            //title
+            Center(
+              child: Text(
+                "$title ${store.name}",
+                style: TextStyle(
+                  fontFamily: "Poppins-Medium",
+                  fontSize: 20,
+                  color: Color(0xFF000000),
+                ),
               ),
             ),
-          ),
 
-          isMap ? Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: ScreenUtil().setWidth(12),
-                vertical: ScreenUtil().setHeight(17)),
-            child: Container(
+            SizedBox(height: ScreenUtil().setHeight(17),),
+        
+            isMap ? Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF252B37),
+                    borderRadius: BorderRadius.all(Radius.circular(
+                      ScreenUtil().setWidth(8),
+                    )),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.12),
+                        offset: Offset(
+                          0.0,
+                          ScreenUtil().setWidth(3.0),
+                        ), //(x,y)
+                        blurRadius: ScreenUtil().setWidth(6.0),
+                      ),
+                    ],
+                  ),
+                  height: ScreenUtil().setHeight(200),
+                  width: ScreenUtil().setWidth(340),
+                  padding: EdgeInsets.all(20),
+                  child: store.location == null
+                      ? Center(child:Text("Google map here shows store's location but the location is null", style: TextStyle(color:Colors.white),))
+                      : Center(child:Text("Google map here shows store's location", style: TextStyle(color:Colors.white)))
+            ) : SizedBox(height: ScreenUtil().setHeight(30),),
+        
+            SizedBox(height: ScreenUtil().setHeight(17),),
+            
+            //items
+            Container(
                 decoration: BoxDecoration(
-                  color: Color(0xFF252B37),
-                  borderRadius: BorderRadius.all(Radius.circular(
-                    ScreenUtil().setWidth(8),
-                  )),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.12),
-                      offset: Offset(
-                        0.0,
-                        ScreenUtil().setWidth(3.0),
-                      ), //(x,y)
-                      blurRadius: ScreenUtil().setWidth(6.0),
-                    ),
-                  ],
-                ),
-                height: ScreenUtil().setHeight(200),
-                width: ScreenUtil().setWidth(340),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(
+                      ScreenUtil().setWidth(8),
+                    )),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.12),
+                        offset: Offset(
+                          0.0,
+                          ScreenUtil().setWidth(3.0),
+                        ), 
+                        blurRadius: ScreenUtil().setWidth(6.0),
+                      ),
+                    ],
+                  ),
                 padding: EdgeInsets.all(20),
-                child: store.location == null
-                    ? Center(child:Text("Google map here shows store's location but the location is null", style: TextStyle(color:Colors.white),))
-                    : Center(child:Text("Google map here shows store's location", style: TextStyle(color:Colors.white)))
-                ),
-          ) : SizedBox(height: ScreenUtil().setHeight(30),),
-
-          //items
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: ScreenUtil().setWidth(12)),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(
-                    ScreenUtil().setWidth(8),
-                  )),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.12),
-                      offset: Offset(
-                        0.0,
-                        ScreenUtil().setWidth(3.0),
-                      ), 
-                      blurRadius: ScreenUtil().setWidth(6.0),
-                    ),
-                  ],
-                ),
-              padding: EdgeInsets.all(20),
-              child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  primary: false,
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: store.items.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          store.items[index].title,
-                          style: TextStyle(
-                            fontFamily: "Poppins-Medium",
-                            fontSize: 12,
-                            color: Color(0xFF252B37),
+                child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    primary: false,
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: store.items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            store.items[index].title,
+                            style: TextStyle(
+                              fontFamily: "Poppins-Medium",
+                              fontSize: 12,
+                              color: Color(0xFF252B37),
+                            ),
                           ),
-                        ),
-                        Text(
-                          store.items[index].quantity,
-                          style: TextStyle(
-                            fontFamily: "Poppins-Medium",
-                            fontSize: 12,
-                            color: Color(0xFf252B37),
+                          Text(
+                            store.items[index].quantity,
+                            style: TextStyle(
+                              fontFamily: "Poppins-Medium",
+                              fontSize: 12,
+                              color: Color(0xFf252B37),
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  }
-                ),
-              
+                        ],
+                      );
+                    }
+                  ),
             ),
-          ),
-
-          Spacer(),
-
-          //button
-          SliderButtonWidget(
-            label: buttonLabel,
-            onAction: onAction!
-          ),
-
-          SizedBox(height: ScreenUtil().setHeight(30)),
-        ],
+        
+            Spacer(),
+        
+            //button
+            SliderButtonWidget(
+              label: buttonLabel,
+              onAction: onAction!
+            ),
+        
+            SizedBox(height: ScreenUtil().setHeight(30)),
+          ],
+        ),
       ),
     );
   }
